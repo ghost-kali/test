76a93201a9a1b4bc4411c72f3adcfd41795c6956
ghost-kali <109295912+ghost-kali@users.noreply.github.com>
2024-07-16 14:16:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)
 def predict():
     
     text = request.get_json().get("message")
-    ip = request.remote_addr
+    ip = request.headers['X-Forwarded-For']
     response = get_response(text)
     message = {"answer": response}
     save_rec(text,response,ip)
