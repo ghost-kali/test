ffc74bb3e2b0150ffa697f2ecc1d9eed8a173d59
ghost-kali <109295912+ghost-kali@users.noreply.github.com>
2024-07-16 13:57:37 +0530

    Update airtable.py

--- airtable.py
+++ airtable.py
@@ -1,6 +1,6 @@
 import requests
-AIRTABLE_BASE_ID = "appONLo1mGUZDlc3o"
-AIRTABLE_API_KEY = "patRtv7tfjiUJuEX0.46b14546ad87b10c91df3b03cfa8a6b763b4732893e317a3bd19300a93fa0133"
+AIRTABLE_BASE_ID = "appB6LFUMhkx681r3"
+AIRTABLE_API_KEY = "patvOdvupRPT8kBRg.9caf5e416b2f998138cc403bbd375b52f8df131c2027e622da894813d5622403"
 AIRTABLE_TABLE_NAME = "chatbot"
 endpoint = f"https://api.airtable.com/v0/{AIRTABLE_BASE_ID}/{AIRTABLE_TABLE_NAME}"
 
@@ -8,11 +8,12 @@ headers = {
 "Authorization": f"Bearer {AIRTABLE_API_KEY}",
 "Content-Type": "application/json"
 }
-def save_rec(user_query,response):
+def save_rec(user_query,response,ip_address):
     data = {
   "records": [
           {
         "fields": {
+            "IP_address": f"{ip_address}",
             "Query": f"{user_query}",
             "Response": f"{response}"
         }
@@ -25,4 +26,4 @@ def save_rec(user_query,response):
 
 
 if __name__ == '__main__':
-    save_rec("hello","hi there")
+    save_rec("hello","hi there","ip")
