0ffbf6ac5e5212cf55c01e8d6fa3f85835cfa805
ghost-kali <109295912+ghost-kali@users.noreply.github.com>
2024-08-14 21:34:01 +0530

    production assistance api

--- assistants.py
+++ assistants.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv,find_dotenv
 from openai import OpenAI
 
 # Enter your Assistant ID here.
-ASSISTANT_ID = "asst_4axLKfoOQrz2f5pQTzzsLE59"
+ASSISTANT_ID = "asst_SGAgwHFwEEahNu6sPjBtywWK"
 load_dotenv(find_dotenv())
 # Make sure your API key is set as an environment variable.
 client = OpenAI()
